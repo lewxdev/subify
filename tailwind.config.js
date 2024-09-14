d4f266b3e45e20945b593ee58b5fb46f3b119876
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-09-14 14:06:08 -0400

    chore: update config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/* eslint-env node */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   mode: "jit",
   darkMode: "class",
   content: ["./**/*.tsx"],
-  plugins: []
-}
+  plugins: [],
+};
