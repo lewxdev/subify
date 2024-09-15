8f87b42fc6c83de319d00d786be78dcc256a0825
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-09-15 10:29:07 -0400

    feat: configure shadcn/ui and add first component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /* eslint-env node */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   mode: "jit",
   darkMode: "class",
   content: ["./src/**/*.tsx"],
-  plugins: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 };
