31347a9aa96d6878e36f9bd0e2b3e3c984e2a1cc
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-10-02 10:15:09 -0400

    feat: create settings tab

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,22 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      keyframes: {
+        shake: {
+          "0%, 100%": {
+            transform: "translateX(0)",
+          },
+          "10%, 30%, 50%, 70%, 90%": {
+            transform: "translateX(-10px)",
+          },
+          "20%, 40%, 60%, 80%": {
+            transform: "translateX(10px)",
+          },
+        },
+      },
+      animation: {
+        shake: "shake 0.6s ease-in-out 0.25s 1",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
