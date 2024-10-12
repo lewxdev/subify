aa2f37e0ab4da1736fa16055e8e1005ec274346c
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-10-12 14:04:54 -0400

    feat: add copy visual feedback
    also:
    - remove unnecessary code related to presets and respective details
    - change icon from clipboard to copy
    - fix issue where empty url in history would still show visual separator
    - change some ui info (like tabs and timestamps) to be un-selectable

--- src/storage/presets.ts
+++ src/storage/presets.ts
@@ -1,16 +1,33 @@
 import { z } from "zod";
 import { createStorageHooks } from "@/storage/utils";
+import { getDomain } from "@/utils";
 
 const schema = z
   .object({
+    presetName: z.enum(["custom", "domain", "timestamp"]).default("domain"),
     custom: z.string().default("custom"),
-    name: z.enum(["custom", "domain", "timestamp"]).default("domain"),
+  })
+  .transform(async (state, ctx) => {
+    switch (state.presetName) {
+      case "custom":
+        return { ...state, detail: state.custom };
+      case "domain":
+        return { ...state, detail: await getDomain() };
+      case "timestamp":
+        return { ...state, detail: Date.now().toString() };
+      default:
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: `unknown preset: "${state.presetName}"`,
+        });
+        return z.NEVER;
+    }
   })
   .default({});
 
-export type PresetName = z.infer<typeof schema>["name"];
+export type PresetName = z.infer<typeof schema>["presetName"];
 
 export const presets = createStorageHooks("presets", schema, {
-  select: (state, payload: PresetName) => ({ ...state, name: payload }),
+  select: (state, payload: PresetName) => ({ ...state, presetName: payload }),
   update: (state, payload: string) => ({ ...state, custom: payload }),
 });
--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,11 @@ import type { z } from "zod";
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
+export const getDomain = async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  return tab?.url ? new URL(tab.url).hostname.replace(/^www\./, "") : "";
+};
+
 export const isMatchingInput = <TInput extends Record<string, z.Primitive>>(
   schema: z.ZodType<unknown, z.ZodObjectDef, TInput>,
   a: TInput,
